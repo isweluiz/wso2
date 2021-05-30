5c9d41a38619ee6318b2e9730fe4109cf07bdd2f
Luiz Eduardo <isweluiz@hotmail.com>
2021-05-30 19:20:56 -0300

    Update wso2-desinstall.sh

--- wso2-desinstall.sh
+++ wso2-desinstall.sh
@@ -4,7 +4,7 @@ killPID=`ps aux | grep wso2 | grep -v grep | awk '{print $2}' | xargs kill -9`
 echo "Eliminando processos..."
 $killPID 
 echo "Removendo arquivos e configurações..."
-rm -rf /etc/init.d/wso2 /etc/systemd/system/wso2.service /opt/apm/ 
+rm -rf /etc/init.d/wso2 /etc/systemd/system/wso2.service /opt/apm/  /etc/motd 
 echo "Deletando usuário..."
 userdel wso2
 echo "Desinstalação completa."
